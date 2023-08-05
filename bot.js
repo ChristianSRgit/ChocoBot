5be9f33b350d6233ccf16e5eebaf8cf70e530514
Christian Ramundo <Christian.s.ramundo@gmail.com>
2023-08-05 15:02:06 -0300

    welcome and start ok,help not working

--- bot.js
+++ bot.js
@@ -1,4 +1,16 @@
 const pc = require('picocolors');
+//console.log(pc.color/bgcolor('hello cosmos'))
+const emoji = require('node-emoji');
+/* emojilib: provides a list of emojis and keyword search on top of it
+skin-tone: parses out base emojis from skin tones */
+
+const {
+  telegramStart,
+  welcomeMessageHelp,
+  ayuda,
+  blank
+  
+} = require('./constants');
 
 
 const TelegramBot = require('node-telegram-bot-api');
@@ -7,8 +19,10 @@ const { GoogleSpreadsheet } = require('google-spreadsheet');
 require('dotenv').config();
 const botToken  = process.env.botToken;
 
+console.log(
 
 
+)
 const bot = new TelegramBot(botToken, { polling: true });
 
 
@@ -18,64 +32,89 @@ const googleSheetId = process.env.GOOGLE_SHEETS_ID;
 
 const doc = new GoogleSpreadsheet(googleSheetId); */
 
-// Matches "/echo [whatever]" 
+
+// Manejar el primer mensaje del usuario para enviar el mensaje de bienvenida
+bot.once('message', (msg) => {
+  const chatId = msg.chat.id;
+
+  // Mensaje de bienvenida
+  const welcomeMessage = `¡Hola ${msg.from.first_name}!\n \n${welcomeMessageHelp}`;
+  
+  // Enviar el mensaje de bienvenida
+  bot.sendMessage(chatId, welcomeMessage);
+  console.log(pc.bgGreen('MENSAJE DE BIENVENIDA ENVIADO'));
+});
 
 
 
+let products = [
+  {
+    id:0,
+    name:'producto1',
+    price:'precio1',
+    stock:'stock1'
+  },
+  {
+    id:1,
+    name:'producto2',
+    price:'precio2',
+    stock:'stock2'
+  },
+  {
+    id:2,
+    name:'producto3',
+    price:'precio3',
+    stock:'stock3'
+  },
+  {
+    id:3,
+    name:'producto4',
+    price:'precio4',
+    stock:'stock4'
+  },
+  {
+    id:4,
+    name:'producto5',
+    price:'precio5',
+    stock:'stock5'
+  }
+];
 
 // Handle the /start command to display available products
 bot.onText(/\/start/, async (msg) => {  // /start
   const chatId = msg.chat.id;
-  const products = [
-    {
-      id:0,
-      name:'producto1',
-      price:'precio1',
-      stock:'stock1'
-    },
-    {
-      id:1,
-      name:'producto2',
-      price:'precio2',
-      stock:'stock2'
-    },
-    {
-      id:2,
-      name:'producto3',
-      price:'precio3',
-      stock:'stock3'
-    },
-    {
-      id:3,
-      name:'producto4',
-      price:'precio4',
-      stock:'stock4'
-    },
-    {
-      id:4,
-      name:'producto5',
-      price:'precio5',
-      stock:'stock5'
-    }
-  ];
+  
+
+  const buttonsRow = [];
+
+  products.forEach((product) => {
+    const button = {
+      text: `${product.name} ${product.price}`,
+      callback_data: product.id.toString(),
+    };
+    buttonsRow.push(button);
+  });
+
 
   // Create a Telegram inline keyboard with product buttons
   const keyboard = {
-    inline_keyboard: products.map((product) => {
-      return [
-        {
-          text: `${product.name} - $${product.price}`,
-          callback_data: product.id.toString(), // Assuming there's an "id" column in the sheet
-        },
-      ];
-    }),
-  };
+    inline_keyboard: [buttonsRow]
+
+  }
 
   // Send the message with the inline keyboard to the user
-  bot.sendMessage(chatId, 'Welcome to our store! Please choose a product:', {
+  bot.sendMessage(chatId,telegramStart, {
     reply_markup: JSON.stringify(keyboard),
   });
   console.log(pc.bgGreen('SHOWING PRODUCTS'))
 });
 
-// Handle button clicks to show product details
+
+
+
+bot.on(/\/help/, async (msg) => {
+  const chatId = msg.chat.id;
+
+  // send a message to the chat acknowledging receipt of their message
+  bot.sendMessage(chatId, `${ayuda}`);
+});
---
+++ constants.js
@@ -0,0 +1,12 @@
+const telegramStart = 'Bienvenido a nuestra tienda  🐷🍫🛒  Por favor, elige un producto:';
+const welcomeMessageHelp = 'Bienvenido a nuestra tienda  🍫🛒🐷\n \nSelecciona: \n/start para empezar \n/help para recibir ayuda';
+const ayuda = 'texto supergenial de ayuda de chocoloc0s';
+const blank =' '
+
+
+module.exports = {
+    telegramStart,
+    welcomeMessageHelp,
+    ayuda,
+    blank
+}
