8f21306d988abf18fadac55f427b8a4b6cd3ce6d
christian Wirenet <christian.s.ramundo@gmail.com>
2023-08-08 13:16:03 -0300

    spreadsheet working, metadata on web

--- js/sheets.js
+++ js/sheets.js
@@ -1,18 +1,60 @@
 const {google} = require('googleapis');
+require('dotenv').config();
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 // https://www.npmjs.com/package/google-spreadsheet
+const express = require('express');
+const app = express();
 
-require('dotenv').config();
 
-
-//deberia hacer aca la auth, get con template string `${variable}` para traer los valores que quiero de las hojas y despues exportarlos
-
-const googleSheetId = process.env.GOOGLE_SHEETS_ID;
-
-// import google sheets
+// import google sheets id
 const credentials = process.env.GOOGLE_SHEETS_CREDENTIALS;
 
 
-const doc = new GoogleSpreadsheet(googleSheetId,credentials);
 
-console.log(doc);
+app.get("/", async (req, res) => {
+    
+    const auth = new google.auth.GoogleAuth({
+        keyFile: "secret.json",
+        scopes: "https://www.googleapis.com/auth/spreadsheets",
+    });
+
+    //create client instance for auth
+    const client = await auth.getClient();
+
+//instance of google sheets API
+
+const googleSheets = google.sheets({ version: "v4", auth: client});
+
+const spreadsheetId  = process.env.GOOGLE_SHEETS_ID;
+
+
+const meta = await googleSheets.spreadsheets.get({
+
+    auth,
+    spreadsheetId,
+
+})
+
+res.send(meta.data)
+
+});
+
+// read rows from spreadsheet
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(6969, (req, res) => console.log(' running on http://localhost:6969'));
+//deberia hacer aca la auth, get con template string `${variable}` para traer los valores que quiero de las hojas y despues exportarlos
+
+
+
+
